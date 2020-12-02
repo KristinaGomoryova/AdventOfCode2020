83a14d38378414e79ddd36bdf5decb0a82a90a50
KristinaGomoryova <dra5lobgic>
2020-12-02 21:57:56 +0100

    puzzle 2

--- src/src_day02.R
+++ src/src_day02.R
@@ -28,3 +28,42 @@ data[which(data$count >= data$lowerRange & data$count <= data$upperRange), "vali
 
 sum(data$valid)
 
+############################################# PUZZLE 2 #####################################################
+
+data2 <- read.delim(here('data', 'day_02.txt'))
+colnames(data2) <- 'V1'
+
+#clean data
+data2 <- data2 %>%
+  separate(V1, into = c("number", "letter", "password"), sep = " ") %>%
+  separate(number, into = c("lowerRange", "upperRange"), sep = "-")
+data2$letter <- gsub( ":", "", data2$letter)
+
+data2$lowerRange <- as.numeric(data2$lowerRange)
+data2$upperRange <- as.numeric(data2$upperRange)
+
+x<- 0
+for (i in 1:nrow(data2)){
+  # print(data2$password_char[[i]][data2$lowerRange[i]])
+  # print(data2$password_char[[i]][data2$upperRange[i]])
+  # print(data2$letter[i])
+  data2[which(((data2$password_char[[i]][data2$lowerRange[i]] == data2$letter[i]))), 'valid_LR'] <- 1
+  print(paste(i, 
+              data2$letter[i], 
+              data2$password_char[[i]][data2$lowerRange[i]], 
+              data2$password_char[[i]][data2$upperRange[i]],
+              data2$password_char[[i]][data2$lowerRange[i]] == data2$letter[i],
+              data2$password_char[[i]][data2$upperRange[i]] == data2$letter[i],
+              data2$password_char[[i]][data2$lowerRange[i]] != data2$letter[i],
+              data2$password_char[[i]][data2$upperRange[i]] != data2$letter[i],
+              '***',
+              (data2$password_char[[i]][data2$lowerRange[i]] == data2$letter[i] &   data2$password_char[[i]][data2$upperRange[i]] != data2$letter[i]),
+              (data2$password_char[[i]][data2$lowerRange[i]] != data2$letter[i] & data2$password_char[[i]][data2$upperRange[i]] == data2$letter[i]),
+              (data2$password_char[[i]][data2$lowerRange[i]] == data2$letter[i] &   data2$password_char[[i]][data2$upperRange[i]] != data2$letter[i]) | (data2$password_char[[i]][data2$lowerRange[i]] != data2$letter[i] & data2$password_char[[i]][data2$upperRange[i]] == data2$letter[i])
+        ))
+  if ((data2$password_char[[i]][data2$lowerRange[i]] == data2$letter[i] &   data2$password_char[[i]][data2$upperRange[i]] != data2$letter[i]) | (data2$password_char[[i]][data2$lowerRange[i]] != data2$letter[i] & data2$password_char[[i]][data2$upperRange[i]] == data2$letter[i])) {
+    x <- x+1
+  }
+}
+
+
